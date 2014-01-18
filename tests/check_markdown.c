020fb5d14e5045dc8931fd6fc0cca96f215b88bd
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-18 02:40:35 -0200

    added tests for markdown parser

---
+++ tests/check_markdown.c
@@ -0,0 +1,36 @@
+/*
+ * balde-cms: A simple CMS built with balde microframework.
+ * Copyright (C) 2014 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+ *
+ * This program can be distributed under the terms of the LGPL-2 License.
+ * See the file COPYING.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <glib.h>
+#include <balde.h>
+
+#include "../markdown.h"
+
+
+void
+test_parse_markdown(void)
+{
+    balde_app_t *app = balde_app_init();
+    gchar *mkd = parse_markdown(app, "### Foo ###\n");
+    g_assert_cmpstr(mkd, ==, "<h3>Foo</h3>");
+    g_free(mkd);
+    balde_app_free(app);
+}
+
+
+int
+main(int argc, char** argv)
+{
+    g_test_init(&argc, &argv, NULL);
+    g_test_add_func("/markdown/parse_markdown", test_parse_markdown);
+    return g_test_run();
+}
